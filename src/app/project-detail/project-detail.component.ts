98abf9e2457b24a9ff3a2967715d2cef69b496da
Root User <root@localhost>
2022-06-28 16:57:14 +0530

    code for pop-up menu

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-project-detail',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-detail.component.css']
 })
 export class ProjectDetailComponent implements OnInit {
+  closeResult: string | undefined;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  getDismissReason(reason: any) {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
